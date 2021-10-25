09f8be97fcd93be06923623b3c7176e7b2602eb1
Elaine Zosa <zosaelai@puhti-login1.bullx>
2021-10-25 12:43:09 +0300

    multilingual text encoder

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,9 @@
 from scipy.stats import pearsonr
-
+import numpy as np
 
 def evaluate_scores(pred_scores, true_scores):
+    pred_scores = np.array(pred_scores)
+    true_scores = np.array(true_scores)
     pearsonr_corr = pearsonr(pred_scores, true_scores)
     print("Pearson-r:", pearsonr_corr[0])
     print("p-value:", pearsonr_corr[1])
