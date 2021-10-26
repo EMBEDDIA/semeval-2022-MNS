f4fbe6da2e616e77cd83b1324e53298dd3316492
Emanuela Boros <emanuela.boros@gmail.com>
2021-10-26 10:01:28 +0200

    add random seed to have the same spit

--- split_data.py
+++ split_data.py
@@ -10,14 +10,14 @@ def split_train_data_batch1():
     # 28 rows of fr data
     test_split = df[df.url1_lang == 'fr']
     # shuffle rows of train split
-    train_split = train_split.sample(frac=1)
+    train_split = train_split.sample(frac=1, random_state=2021)
     # take the first 265 rows: 265+28 = 293 (10% 0f 2932)
     test_split = pd.concat([test_split, train_split[:265]])
     train_split = train_split[265:]
     print('train size:', train_split.shape[0])
     print('test size:', test_split.shape[0])
-    #train size: 2646
-    #test size: 293
+    # train size: 2646
+    # test size: 293
     #train_split.to_csv("train_split_batch1.csv", index=False)
     #test_split.to_csv("test_split_batch1.csv", index=False)
     return train_split, test_split
@@ -45,6 +45,6 @@ def sanity_check():
         j = [f for f in files if 'json' in f]
         html_files.extend(h)
         json_files.extend(j)
-    art_ids_json = [i+".json" for i in art_ids]
+    art_ids_json = [i + ".json" for i in art_ids]
     diff = set(art_ids_json).difference(set(json_files))
     print("missing files:", len(diff))
